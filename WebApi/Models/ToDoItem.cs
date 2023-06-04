d15db26d8393d7d08cf35d347a5c7f4a5fa7639e
Waqar Kabir <waqarkabi@hotmail.com>
2023-06-04 12:36:27 +0500

    Added ToDoItem Class in Models

---
+++ WebApi/Models/ToDoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApi.Models
+{
+    public class ToDoItem
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+
+    }
+}
